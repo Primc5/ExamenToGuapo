cd0273a3e6bfb7700133fc20b91524502426a205
Primc <pablocruzmendez@gmail.com>
2018-06-19 11:48:04 +0200

    pods

---
+++ Examenv2/DataHolder.swift
@@ -0,0 +1,13 @@
+//
+//  DataHolder.swift
+//  Examenv2
+//
+//  Created by PABLO CRUZ MENDEZ on 19/6/18.
+//  Copyright © 2018 PABLO CRUZ MENDEZ. All rights reserved.
+//
+
+import UIKit
+
+class DataHolder: NSObject {
+
+}
