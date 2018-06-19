e48b4d584e97da0d323d9b428f978a2f659644bb
Primc <pablocruzmendez@gmail.com>
2018-06-19 11:32:47 +0200

    Initial Commit

---
+++ Examenv2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Examenv2
+//
+//  Created by PABLO CRUZ MENDEZ on 19/6/18.
+//  Copyright © 2018 PABLO CRUZ MENDEZ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
