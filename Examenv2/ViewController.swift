c86c73b8877e9191d7fbea8ae62900641d3ad634
Primc <pablocruzmendez@gmail.com>
2018-06-19 11:39:52 +0200

    botones

--- Examenv2/ViewController.swift
+++ Examenv2/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var Email: UITextField?
+    @IBOutlet var Password: UITextField?
+    
+    @IBOutlet var Registro: UIButton?
+    @IBOutlet var Aceptar: UIButton?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
